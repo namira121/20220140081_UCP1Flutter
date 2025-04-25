843145bd22cdbb9afe55615b85b046d29229492b
Namira Pramadhani <namnam6617@gmail.com>
2025-04-25 15:36:22 +0700

    menambahkan circleavatar, nama user dan icon logout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 70),
+              color: Colors.blue.shade100,
+              child: 
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/cinamonroll2.png'),
+                  ),
+                  const SizedBox(width: 15),
+                  Column(
+                    children: [
+                      Text('Selamat datang,'),
+                      Text('${widget.email}')
+                    ],
+                  ),
+                  IconButton(onPressed: (){
+                    Navigator.pushNamed(context, '/login');
+                  }, icon: Icon(Icons.logout))
+                  
+                ],
+              ),
+            )
+          ],
+        ),),
+      
+    );
   }
 }
