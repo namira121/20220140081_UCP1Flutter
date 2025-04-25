4b872410a70bfe3aba8a315f5352aebb913ca48e
Namira Pramadhani <namnam6617@gmail.com>
2025-04-25 14:04:36 +0700

    menambahkan textformfield dan validator untuk email dan password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -24,7 +24,18 @@ class _LoginPageState extends State<LoginPage> {
             Text('Email'),
             TextFormField(
               controller: emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Enter your email'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },              
+            ),
+            Text('Password'),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Enter your password'),
               validator: (value){
                 if (value == null || value.isEmpty) {
                   return 'Please enter your email';
--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class MyApp extends StatelessWidget {
       initialRoute: '/login',
       routes: {
         '/login' : (context)=> const LoginPage(),
-        
       },
     );
   }
