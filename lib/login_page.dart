09cbfc022e4e77520b3ed2e708c9b16d6aa30713
Namira Pramadhani <namnam6617@gmail.com>
2025-04-25 13:59:16 +0700

    membuat file dan controller untuk email&password.

---
+++ lib/login_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final _formkey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            Image(image: AssetImage('assets/images/pompompurin.png')),
+            Text('Selamat Datang Kembali'),
+            Text('Email'),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },              
+            )
+          ],
+        ),),
+    );
+  }
+}
