01a107b536579cf2a46086ffa63ff6beb4fbd15e
Namira Pramadhani <namnam6617@gmail.com>
2025-04-27 23:24:26 +0700

    membuat appbar untuk title piket gudang

--- lib/data_piket.dart
+++ lib/data_piket.dart
@@ -10,6 +10,13 @@ class DataPiket extends StatefulWidget {
 class _DataPiketState extends State<DataPiket> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Piket Gudang', style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade100,
+      ),
+      
+    );
   }
 }
