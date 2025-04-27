f7d4583b9cd8228ac6be21cb505202819bafb09b
Namira Pramadhani <namnam6617@gmail.com>
2025-04-27 23:40:52 +0700

    membuat Form untuk body halaman datapiket

--- lib/data_piket.dart
+++ lib/data_piket.dart
@@ -10,12 +10,19 @@ class DataPiket extends StatefulWidget {
 class _DataPiketState extends State<DataPiket> {
   @override
   Widget build(BuildContext context) {
+    final _formkey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Piket Gudang', style: TextStyle(fontWeight: FontWeight.bold),),
         centerTitle: true,
         backgroundColor: Colors.blue.shade100,
       ),
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: EdgeInsets.all(16.0)
+          )
+        ),
       
     );
   }
