663b7f60ff30ff6fc7e4f56bfdec3409910e46ee
Namira Pramadhani <namnam6617@gmail.com>
2025-04-29 00:17:47 +0700

    menambahkan tanggal, nama dan tugas sebagai title halaman

--- lib/detail_piket.dart
+++ lib/detail_piket.dart
@@ -14,10 +14,46 @@ class DetailPiket extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail ${tugas}'),
+        title: Text('Detail $tugas'),
         centerTitle: true,
         backgroundColor: Colors.blue.shade100,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 170,
+                  height: 50,
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.indigo.shade100,
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: Text('$tanggal'),
+                ),
+                Container(
+                  width: 170,
+                  height: 50,
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.indigo.shade100,
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: Text('$nama'),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                
+              ],
+            )
+          ],
+        ),),
     );
   }
 }
