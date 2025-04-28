2d5cc156644dcd376e8c5eee264586a34b4efe95
Namira Pramadhani <namnam6617@gmail.com>
2025-04-29 00:52:27 +0700

    membuat appbar untuk data pelanggan

--- lib/daftarcust.dart
+++ lib/daftarcust.dart
@@ -10,6 +10,19 @@ class DataCustomer extends StatefulWidget {
 class _DataCustomerState extends State<DataCustomer> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Pelanggan'),
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade100,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            
+          ],
+        ),),
+    );
   }
 }
