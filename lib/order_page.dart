52b0a8d3523442517f5e455c99b5d390158a1b24
Namira Pramadhani <namnam6617@gmail.com>
2025-04-29 18:58:16 +0700

    membuat appbar untuk halaman data barang

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -8,8 +8,30 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  final TextEditingController tgltransaksiController = TextEditingController();
+  final TextEditingController jenistransaksiController = TextEditingController();
+  final TextEditingController jenisbarangController = TextEditingController();
+  final TextEditingController jumlahbarangController = TextEditingController();
+  final TextEditingController hargaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pendataan Barang'),
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade100,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              
+            ],
+          )),),
+    );
   }
 }
